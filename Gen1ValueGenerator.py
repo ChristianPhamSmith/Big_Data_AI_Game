183205f8cb55ea74694ccd39d164af2b0c5cf571
Christian Pham-Smith <87046619+ChristianPhamSmith@users.noreply.github.com>
2024-03-07 19:21:07 -0800

    Update Gen1ValueGenerator.py
    Added more jump values and also distance sensor points to the gen 1 AI Generator

--- Gen1ValueGenerator.py
+++ Gen1ValueGenerator.py
@@ -15,6 +15,17 @@ for ai in range(1000):
     # This generates a random value that the AI will use to define where on the X-axis it should make a specific decision
     leftXValue = random.randrange(-1280, 1280)
     rightXValue = random.randrange(leftXValue, 1280)
+    
+    
+    nearLeftJump = random.randrange(-1278, 0)
+    midLeftJump = random.randrange(-1279, nearLeftJump)
+    farLeftJump = random.randrange(-1280, midLeftJump)
+    if nearLeftJump == 0:
+        nearRightJump = random.randrange(1, 1278)
+    else:
+        nearRightJump = random.randrange(0, 1278)
+    midRightJump = random.randrange(nearRightJump, 1279)
+    farRightJump = random.randrange(midRightJump, 1280)
 
     # This sets a random movement direction for the three possible movements of go left, go right, and go nowhere.
     # 1 = left, 2= right, 3 = nowheres
@@ -28,12 +39,15 @@ for ai in range(1000):
     jump1 = random.randrange(0, 2)
     jump2 = random.randrange(0, 2)
     jump3 = random.randrange(0, 2)
+    jump4 = random.randrange(0, 2)
+    jump5 = random.randrange(0, 2)
+    jump6 = random.randrange(0, 2)
     
     # This creates a cursor
     mycursor = db.cursor()
     
     # This puts an SQL insert statement into the cursor
-    mycursor.execute(f"INSERT INTO ai_save (generation, score, movement1, movement2, movement3, leftXValue, rightXValue, jump1, jump2, jump3) VALUES (1, 9000, {movement1}, {movement2}, {movement3}, {leftXValue}, {rightXValue}, {jump1}, {jump2}, {jump3})")
+    mycursor.execute(f"INSERT INTO ai_save (generation, score, movement1, movement2, movement3, leftXValue, rightXValue, jump1, jump2, jump3, jump4, jump5, jump6, nearLeftJump, midLeftJump, farLeftJump, nearRightJump, midRightJump, farRightJump) VALUES (1, 9000, {movement1}, {movement2}, {movement3}, {leftXValue}, {rightXValue}, {jump1}, {jump2}, {jump3}, {jump4}, {jump5}, {jump6}, {nearLeftJump}, {midLeftJump}, {farLeftJump}, {nearRightJump}, {midRightJump}, {farRightJump})")
     
     # This commits the statement into the database
     db.commit()
